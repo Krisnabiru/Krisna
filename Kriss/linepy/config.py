5a5110b80558a8234914a33297e9c9986fcdba99
Krisnabiru <31961209+Krisnabiru@users.noreply.github.com>
2018-06-07 15:01:38 +0700

    Update config.py

--- Kriss/linepy/config.py
+++ Kriss/linepy/config.py
@@ -31,7 +31,7 @@ class Config(object):
     APP_TYPE    = ApplicationType._VALUES_TO_NAMES[368]
     APP_VER     = '8.4.1'
     CARRIER     = '51089, 1-0'
-    SYSTEM_NAME = 'HelloWorld'
+    SYSTEM_NAME = 'Krisnabiru'
     SYSTEM_VER  = '8.22.17'
     IP_ADDR     = '8.8.8.8'
     EMAIL_REGEX = re.compile(r"[^@]+@[^@]+\.[^@]+")
